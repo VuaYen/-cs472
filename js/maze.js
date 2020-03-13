2ac6821d23724837e44fe9c1c63d8686d580b70a
VuaYen <yen637@gmail.com>
2020-03-13 00:28:41 -0500

    200313

---
+++ js/maze.js
@@ -0,0 +1,34 @@
+($(document).ready(function() {
+    let i=false;
+    let j=false;
+    $("#start").click(function () {
+        $("#maze").mouseleave(function () {
+            if(!j) {
+                $(".boundary").addClass("youlose");
+                $("#status").text("Sorry, You are Cheating!");
+                i = true;
+                j=true;
+            }
+        });
+        $(".boundary").mouseover(function () {
+            if(!j) {
+                $(".boundary").addClass("youlose");
+                $("#status").text("Sorry, You Lost!");
+                i = true;
+                j=true;
+            }
+        });
+        $("#end").click(function () {
+            if(!i) {
+                $("#status").text("You Win!" );
+                j=true;
+            }
+        });
+        $("#start").click(function () {
+            $(".boundary").removeClass("youlose");
+            $("#status").text("Here We go Again!");
+            i=false;
+            j=false;
+        });
+    });
+})());
